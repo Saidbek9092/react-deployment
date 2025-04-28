a3885a2ec83ee28e25941fe5b9f563fc05c5e75e
Saidbek9092 <saidbek9092@gmail.com>
2025-04-29 00:34:11 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
                 count is {count}
             </button>
             <p>
-                Saidbek
+                newew
             </p>
             <p>
                 Edit <code>src/App.tsx</code> and save to test HMR
